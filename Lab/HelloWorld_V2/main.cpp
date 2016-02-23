18fac9ee30093a0db34977272b96485244dc51a9
Dimitrios <dimitriosgizas@gmail.com>
2016-02-23 12:51:31 -0800

    Signed-off-by: Dimitrios <dimitriosgizas@gmail.com>

---
+++ Lab/HelloWorld_V2/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Dimitrios Gizas
+ *Created on February 17, 2016, 9:45 AM
+ * Purpose: Our First Program 
+ */
+//system Libraries
+#include <iostream>
+
+//using namespace std;
+
+//User Libraries
+  
+//Global Constants 
+  
+//Function Prototypes 
+
+//Execution Begins Here!
+  
+       
+int main(int argc, char** argv) {
+
+    //Declare variables 
+    
+    //Initialize variables 
+   
+    //Input Values 
+    
+    //Map the Inputs to the outputs 
+    
+    //Output the results 
+    std::cout<<"Hello World"<<std::endl;
+    
+    //exit Stage Right! 
+    return 0;
+}
+
