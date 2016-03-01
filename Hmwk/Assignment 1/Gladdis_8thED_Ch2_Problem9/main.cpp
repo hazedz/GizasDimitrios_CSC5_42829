1aa12df241ea8f05839d615cb070d904dda56d98
Dimitrios <dimitriosgizas@gmail.com>
2016-03-01 14:17:22 -0800

    Signed-off-by: Dimitrios <dimitriosgizas@gmail.com>

---
+++ Hmwk/Assignment 1/Gladdis_8thED_Ch2_Problem9/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Dimitrios Gizas
+ * Purpose: Cyborg Data Type Sizes 
+ * Created on March 1, 2016, 2:02 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+  
+//Global Constants 
+  
+//Function Prototypes 
+
+
+
+
+
+
+
+
+//Execution
+
+int main(int argc, char** argv) {
+
+    // Cyborg Data Types
+    
+    cout << "Size of a character is: " << sizeof(char) << " byte(s)." <<
+            endl;
+    
+    cout << "Size of an integer is: " << sizeof(int) << " byte(s)." <<
+            endl;
+    
+    cout << "Size of a float is: " << sizeof(float) << " byte(s)." <<
+            endl;
+    
+    cout << "Size of a double is: " << sizeof(double) << " byte(s)." << 
+            endl;
+    
+
+    return 0;
+}
+
