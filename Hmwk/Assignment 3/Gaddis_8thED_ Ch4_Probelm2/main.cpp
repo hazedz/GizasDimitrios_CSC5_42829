91cd7e630491be8233df9736431f2376ac551b2d
Dimitrios <dimitriosgizas@gmail.com>
2016-03-28 10:54:08 -0700

    Signed-off-by: Dimitrios <dimitriosgizas@gmail.com>

---
+++ Hmwk/Assignment 3/Gaddis_8thED_ Ch4_Probelm2/main.cpp
@@ -0,0 +1,60 @@
+/* 
+ * File:   main.cpp
+ * Author: Dimitrios Gizas
+ * Created on March 28, 2016, 10:15 AM
+ * Purpose: 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    int num1;
+    char romanN;
+    
+    cout << "Enter a number between 1 and 10 to convert it into Roman Numeral" 
+            << endl;
+    cin >> num1;
+    
+    switch (num1) {
+    
+        case 1 : cout << "I" << endl;
+        break;
+        case 2 : cout << "II" << endl;
+        break;
+        case 3 : cout << "III" << endl;
+        break;
+        case 4 : cout << "IV" << endl;
+        break;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
+
---
+++ Hmwk/Assignment 3/Gaddis_8thED_Ch4_Problem1/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Dimitrios Gizas
+ * Created on March 28, 2016, 10:15 AM
+ * Purpose: 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   //Declare variables 
+    int num1, num2;
+    
+   //Asking for the 2 numbers 
+    cout << "Please type in your First number" << endl;
+    cin >> num1;
+    
+    cout << "Please type in you Second number" << endl;
+    cin >> num2; 
+    
+    //output results 
+    if (num1 > num2) cout << num1 << " is larger then " << num2;
+    if (num1 < num2) cout << num2 << " is larger then " << num1; 
+    
+    return 0;
+}
+
