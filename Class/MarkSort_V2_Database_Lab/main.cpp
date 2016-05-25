816c24df96b248c749671d058c091871f5210584
Dimitrios <dimitriosgizas@gmail.com>
2016-05-25 09:14:13 -0700

    Signed-off-by: Dimitrios <dimitriosgizas@gmail.com>

--- Class/MarkSort_V2_Database_Lab/main.cpp
+++ Class/MarkSort_V2_Database_Lab/main.cpp
@@ -17,10 +17,10 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
-void mrkSort(int [],int [],int);//Sort the whole array
-void prntAry(int [],int,int);//Print the array with columns
-void prntAry(int [],int [],int,int);//Print the array with columns
-void fillAry(int [],int[],int);//Fill array with 2 digit random numbers
+void mrkSort(int *,int *,int);//Sort the whole array
+void prntAry(int *,int,int);//Print the array with columns
+void prntAry(int *,int *,int,int);//Print the array with columns
+void fillAry(int *,int *,int);//Fill array with 2 digit random numbers
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
@@ -56,39 +56,39 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void mrkSort(int a[],int indx[],int n){
+void mrkSort(int *a,int *indx,int n){
     for(int i=0;i<n-1;i++){
         for(int j=i+1;j<n;j++){
-            if(a[indx[i]]>a[indx[j]]){
-                indx[i]=indx[i]^indx[j];
-                indx[j]=indx[i]^indx[j];
-                indx[i]=indx[i]^indx[j];
+            if(*(a+*(indx+i))>(*(a+*(indx+j)))){
+                *(indx+i)=*(indx+i)^*(indx+j);
+                *(indx+j)=*(indx+i)^*(indx+j);
+                *(indx+i)=*(indx+i)^*(indx+j);
             }
         }
     }
 }
 
-void prntAry(int b[],int indx[],int m, int perLine){
+void prntAry(int *b,int *indx,int m, int perLine){
     cout<<endl<<endl;
     for(int i=0;i<m;i++){
-        cout<<b[indx[i]]<<" ";
+        cout<<*(b+*(indx+i))<<" ";
         if(i%perLine==(perLine-1))cout<<endl;
     }
     cout<<endl<<endl;
 }
 
-void prntAry(int b[],int m, int perLine){
+void prntAry(int *b,int m, int perLine){
     cout<<endl<<endl;
     for(int i=0;i<m;i++){
-        cout<<b[i]<<" ";
+        cout<<*(b+i)<<" ";
         if(i%perLine==(perLine-1))cout<<endl;
     }
     cout<<endl<<endl;
 }
 
-void fillAry(int a[],int indx[],int n){
+void fillAry(int *a,int indx[],int n){
     for(int i=0;i<n;i++){
-        a[i]=rand()%90+10;//2 digit random number
-        indx[i]=i;
+        *(a+i)=rand()%90+10;//2 digit random number
+        *(indx+i)=i;
     }
 }
